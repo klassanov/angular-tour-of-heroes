5ceca6eadb8bed20e16ab3ce4ac9d2b20cb40f21
Alexander Klassanov <klassanov@live.com>
2023-06-26 12:02:45 +0300

    Tutorial point 6. Add services completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   getHeroes(): void {
@@ -23,7 +25,7 @@ export class HeroesComponent {
     //async call => subscribe for it (callback)
     //result is the emitted array from the service
     this.heroService.getHeroes()
-        .subscribe(result => this.heroes = result);
+      .subscribe(result => this.heroes = result);
 
   }
 
@@ -33,5 +35,6 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`Heroes Component: selected hero id = ${hero.id}`);
   }
 }
